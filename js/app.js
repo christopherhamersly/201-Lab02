287a680b133fcc074c4c936bd931f99ee3682b99
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-04-28 16:15:23 -0700

    started java coding, finished css styling, updated html coding

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+'use strict';
+// initial question
+var mountainsOrBeach = prompt('Do you like the mountains');
 
-// alert ('Im alive');
+if(mountainsOrBeach.toLowerCase() === 'yes'){
+  prompt('Is Mt. Rainier your favorite Mountain?');
+} else if (mountainsOrBeach.toLowerCase() === 'no') {
+  prompt('Do you prefer the beach?');
+} else {
+    prompt('Please answer either yes or no')
+}
+
+// Question Two
+var mtRainier = prompt ('Do you like hiking in the Cascades?');
+
+if(mtRainier.toLowerCase() === 'yes'){
+  prompt('Do you like staying overnight?');
+} else if (mtRainier.toLowerCase() === 'no'){
+  prompt ('Do you prefer the Olympics?')
+} else {
+    prompt('Please answer either yes or no')
+}
+
+var camping = prompt('Do you hike in more than four miles?');
 
