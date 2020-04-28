00e28f3e29ce0c63c1831c9866c4cc4621bd7c8d
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-04-28 16:50:32 -0700

    added questions to javascript

--- js/app.js
+++ js/app.js
@@ -1,25 +1,44 @@
 'use strict';
-// initial question
-var mountainsOrBeach = prompt('Do you like the mountains');
+
+var userName = prompt ("What is your name?")
+
+var mountainsOrBeach = prompt(userName + ', ' + 'do you like the mountains?');
 
 if(mountainsOrBeach.toLowerCase() === 'yes'){
-  prompt('Is Mt. Rainier your favorite Mountain?');
+  prompt( userName + ' ' + 'is Mt. Rainier your favorite Mountain?');
 } else if (mountainsOrBeach.toLowerCase() === 'no') {
-  prompt('Do you prefer the beach?');
+  prompt( 'Or' + userName + ' ' + 'Do you prefer the beach?');
 } else {
-    prompt('Please answer either yes or no')
+    prompt(userName + ', ' + 'please answer either yes or no' )
 }
 
-// Question Two
-var mtRainier = prompt ('Do you like hiking in the Cascades?');
+var cascades = prompt ('Do you like hiking in the Cascades?');
 
-if(mtRainier.toLowerCase() === 'yes'){
+if(cascades.toLowerCase() === 'yes'){
   prompt('Do you like staying overnight?');
-} else if (mtRainier.toLowerCase() === 'no'){
+} else if (cascades.toLowerCase() === 'no'){
   prompt ('Do you prefer the Olympics?')
 } else {
-    prompt('Please answer either yes or no')
+    prompt(userName + ', ' + 'please answer either yes or no' )
 }
 
-var camping = prompt('Do you hike in more than four miles?');
+var hiking = prompt('When you hike, do you hike than four miles?');
+
+  if(camping.toLowerCase() === 'yes'){
+  prompt(userName + ' ' + 'have you ever thought about hiking across Washington?');
+} else if (camping.toLowerCase() === 'no'){
+  prompt(userName + ' ' + 'I don\'t blame you, I worry aout getting lost');
+} else {
+    prompt (userName + ', ' + 'Please answer either yes or no' )
+}
+
+var campFires = prompt(userName + ', ' + 'do you like the smell of campfires?')
+
+  if(campFires.toLowerCase() === 'yes'){
+prompt('Hey,' + userName + ' ' + 'I do too.');
+} else if (campFires.toLowerCase() === 'no'){
+  prompt (userName + ', ' + 'thats ok, I can see how someone wouldn\'t like them.');
+} else {
+  prompt (userName + ', ' + 'Please answer either yes or no' )
+}
 
